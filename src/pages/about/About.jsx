0496c71b75483c985183ab0b860ea7813bd7b402
opesnoir <goodsurroundsme@outlook.com>
2022-12-29 18:36:26 +0100

    over mij pagina opgezet en van styling voorzien. Pagina is helemaal geworden hoe ik het wil. Als ik de overige pagina's af heb, moet ik ze aan deze pagina toetsen. Hier is namelijk alles (denk aan de padding, margin, lettertype grote, zin-afstand etc.) hoe het is beoogd.

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import styles from './About.module.css'
+import styles from './About.module.css';
+import imgSheep from '../../assets/aboutme-sheep-pexels-arthur-swiffen-12550240.jpg'
 
 const About = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <section>
+                <div className={styles.aboutOuterContainer}>
+                    <div className={styles.aboutInnerContainer}>
+                        <div>
+                            <h1 className={styles.aboutTitel}>Over mij</h1>
+                        </div>
+                        <div>
+                            <p className={styles.aboutTekst}>“Uw woord is een lamp voor mijn voet, en een licht op mijn pad.” (Psalm 119:105). Dat
+                                schreef David om het woord van de HEERE te omschrijven. Voor mij betekent Gods woord
+                                hetzelfde. Daarom heb ik ervoor gekozen voor mijn afstudeer opdracht deze
+                                Bijbelapplicatie te bouwen.</p>
+                            <p className={styles.aboutTekst}>Mijn geloof in JHWH vormt het fundament van mijn bestaan en Zijn woord is mijn morele
+                                kompas. Ik hoop dan ook dat de Bijbel jou net zo zal inspireren en dat het een licht
+                                voor jou
+                                voeten mag zijn.</p>
+                            <p className={styles.aboutNaam}>Mirre</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <img src={imgSheep} alt="Afbeelding van een Schaap" className={styles.aboutImg}/>
+        </>
     );
 };
 
