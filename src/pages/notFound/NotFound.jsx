8a79aece7c9ba885093fe94404fd635bfc31ade5
opesnoir <goodsurroundsme@outlook.com>
2022-12-29 14:12:03 +0100

    img aangepast, code opgeschoond. De pagina werkt en is qua vormgeving precies hoe ik het beoog. De pagina is klaar

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,36 +1,33 @@
 import React from 'react';
 import styles from './NotFound.module.css';
 import imgRam from '../../assets/pagenotfound-ram-pexels-johannes-havn-3218340-2.jpg';
-
-
-
 import {useNavigate} from "react-router-dom";
 
 const NotFound = () => {
 
-  /*  const navigate = useNavigate()
+    const navigate = useNavigate()
     setTimeout(() => {
         navigate(-1)
-    }, 3000)*/
+    }, 3000)
 
     return (
         <>
             <section>
-                <div className={styles.notFoundOuterContainer} >
+                <div className={styles.notFoundOuterContainer}>
                     <div className={styles.notFoundInnerContainer}>
                         <div>
-                            <h1 className={styles.title}><span className={styles.title404}>404|</span>Pagina niet gevonden</h1>
+                            <h1 className={styles.title}><span className={styles.title404}>404|</span>Pagina niet
+                                gevonden</h1>
                         </div>
                         <div>
-                            <p className={styles.text}>De pagina die je zoekt bestaat niet. Je wordt binnen 3 seconden teruggestuurd naar de
+                            <p className={styles.text}>De pagina die je zoekt bestaat niet. Je wordt binnen 3 seconden
+                                teruggestuurd naar de
                                 vorige pagina.</p>
                         </div>
                     </div>
                 </div>
             </section>
-            <div>
                 <img className={styles.imgRam} src={imgRam} alt="Afbeelding van een Ram"/>
-            </div>
         </>
     );
 };
