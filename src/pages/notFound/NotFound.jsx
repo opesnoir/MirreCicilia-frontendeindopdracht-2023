292e19a5f99f8a38ab942ad2a77fb483e436e435
opesnoir <goodsurroundsme@outlook.com>
2022-12-29 13:40:20 +0100

    page not found achtergrond aangepast, naar donkerdere kleur. Margin bottem verwijderd zodat de afbeelding en tekstbox tegenelkaar aankomen.

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './NotFound.module.css';
 import imgRam from '../../assets/pagenotfound-ram-pexels-johannes-havn-3218340-2.jpg';
+
+
+
 import {useNavigate} from "react-router-dom";
 
 const NotFound = () => {
 
-/*    const navigate = useNavigate()
+  /*  const navigate = useNavigate()
     setTimeout(() => {
         navigate(-1)
     }, 3000)*/
