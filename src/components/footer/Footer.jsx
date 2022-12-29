b8ce507b27f669d17bbd00f84cc4b685914a1040
opesnoir <goodsurroundsme@outlook.com>
2022-12-29 17:56:43 +0100

    navigatie opgezet inclusief icons en styling vormgegeven, logo in het midden geplaatst en een lichtere kleur gegeven om het vreugdevoller te maken. De ruimte weten te realiseren waar ik voor ging.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import styles from './Footer.module.css';
+import {FiFacebook} from 'react-icons/fi';
+import {FiInstagram} from 'react-icons/fi';
+import {FaTwitter} from 'react-icons/fa';
+import {FaPinterest} from "react-icons/fa";
+
 
 const Footer = () => {
     return (
-        <div>
+        <>
+            <div className={styles.footerOuterContainer}>
+                <div>
+                    <p>Meer informatie:</p>
+                    <Link to="/about" className={styles.footerLink}> Jubilee BibleApp </Link>
+                </div>
+                <div className={styles.footerIcons}>
+                    <p> Bezoek: <FiFacebook/> <FiInstagram/> <FaTwitter/> <FaPinterest/>   </p>
+                    <p className={styles.footerItemYear}> &copy; 2023 </p>
+                </div>
 
-        </div>
+            </div>
+        </>
     );
 };
 
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import styles from './NavBar.module.css';
+import {NavLink} from "react-router-dom";
+import JubileeLogo from '../../assets/JubileeBibleApp-transparant.png';
+import {AiOutlineUser} from 'react-icons/ai';
+import {AiOutlineSearch} from "react-icons/ai";
 
-const NavBar = () => {
+const NavBar = ({auth}) => {
+
+    const navLink = ({isActive}) => isActive
+        ? 'active-link'
+        : 'default-link'
+
     return (
-        <div>
-
-        </div>
+        <>
+            <nav>
+                <div className={styles.navBarContainer}>
+                    <ul>
+                        <li><NavLink to="/" className={navLink}>Home</NavLink></li>
+                        <li><NavLink to="/about" className={navLink}>Over</NavLink></li>
+                        <img src={JubileeLogo} alt="Jubilee BibleApp Logo" className={styles.navBarLogo}/>
+                        <li><NavLink to="/register" className={navLink}> <AiOutlineUser/> Registreer / Login</NavLink></li>
+                        <li><NavLink to="/search" className={navLink}> <AiOutlineSearch/> Zoek</NavLink></li>
+                        {auth && <li><NavLink to="/profile">Profielpagina</NavLink></li>}
+                    </ul>
+                </div>
+            </nav>
+        </>
     );
 };
 
