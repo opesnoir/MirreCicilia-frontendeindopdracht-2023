1dd1724809438cab180a21b9d79b0208d930a5fe
opesnoir <goodsurroundsme@outlook.com>
2022-12-30 18:01:22 +0100

    wachtwoord tonen checkboxen toegevoegd en tekst aangepast

--- src/components/RegisterInput/RegisterInput.jsx
+++ src/components/RegisterInput/RegisterInput.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from '../RegisterInput/RegisterInput.module.css';
 
 
-const RegisterInput = ({name, label, required, type, placeholder, validationSchema, errors, register }) => {
+const RegisterInput = ({name, children, label, required, type, placeholder, validationSchema, errors, register }) => {
     return (
         <>
             <div className={styles.registerForminput}>
             <label htmlFor={name}>
                 {label}
                 {required}
+
             </label>
             <input className={styles.registerFormInputTekst}
                 id={name}
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,13 +2,18 @@ import React, {useRef} from 'react';
 import styles from './Register.module.css';
 import RegisterInput from '../../components/RegisterInput/RegisterInput';
 import {useForm} from 'react-hook-form';
-import registerImgSheep from '../../assets/register-sheep-pexels-trinity-kubassek-288621.jpg'
+import registerImgSheep from '../../assets/register-sheep-pexels-trinity-kubassek-288621.jpg';
+import {useState} from "react";
+import {FiEyeOff} from 'react-icons/fi';
+import {FiEye} from "react-icons/fi";
 // add noValidate to form, to remove default error messages
 
 const Register = () => {
     const {register, handleSubmit, reset, watch, formState: {errors}} = useForm();
     const password = useRef({}); // it returns an object called current
     password.current = watch("password", " ");
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmedPassword, setShowConfirmedPassword] = useState(false)
 
     const onSubmit = (data) => {
         console.log(data);
@@ -58,12 +63,15 @@ const Register = () => {
                             <RegisterInput
                                 id="password"
                                 name="password"
-                                type="password"
+                                type={showPassword ? "text" : "password"}
                                 placeholder="Wachtwoord:"
                                 errors={errors}
                                 register={register}
                                 validationSchema={{
-                                    required: "Wachtwoord is vereist",
+                                    required:{
+                                        value: true,
+                                        message:"Wachtwoord is vereist"
+                                    },
                                     pattern: {
                                         value: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
                                         message: "Het wachtwoord bevat minimaal 8 tekens, waarvan 1 hoofdletter, 1 getal en 1 leesteken"
@@ -75,10 +83,16 @@ const Register = () => {
                                 }}
                                 required
                             />
+                            <label>
+                                <input type="checkbox"
+                                checked={showPassword}
+                                       onChange={()=> setShowPassword(!showPassword)}
+                                /> Wachtwoord tonen
+                            </label>
                             <RegisterInput
                                 id="confirmpassword"
                                 name="confirmpassword"
-                                type="password"
+                                type={showConfirmedPassword ? "text" : "password"}
                                 placeholder="Bevestig wachtwoord:"
                                 errors={errors}
                                 register={register}
@@ -92,6 +106,12 @@ const Register = () => {
                                 }}
                                 required
                             />
+                            <label>
+                                <input type="checkbox"
+                                       checked={showConfirmedPassword}
+                                       onChange={()=> setShowConfirmedPassword(!showConfirmedPassword)}
+                                /> Bevestigde wachtwoord tonen
+                            </label>
                             <div className={styles.registerButtonContainer}>
                                 <button type="submit" className={styles.registerButton}>Registreren</button>
                             </div>
