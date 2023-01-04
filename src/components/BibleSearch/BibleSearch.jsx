b677f1f6bd65156ed7734dc451284b7277e87ece
opesnoir <goodsurroundsme@outlook.com>
2023-01-04 08:56:29 +0100

    async functie herschreven, hij gaf namelijk continu errors

--- src/components/BibleSearch/BibleSearch.jsx
+++ src/components/BibleSearch/BibleSearch.jsx
@@ -1,15 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import styles from '../../components/BibleSearch/BibleSearch.module.css';
 
 const BibleSearch = () => {
-
-    const [bibleId, setBibleId] = useState(' ');
-    const [searchTerm, setSearchTerm] = useState(' ');
+    const [bibleId, setBibleId] = useState('english-standard-version');
+    const [searchTerm, setSearchTerm] = useState('Jesus');
     const [searchResults, setSearchResults] = useState([]);
     const [error, setError] = useState(null);
-    const [bibleIds, setBibleIds] = useState([])
+    const [bibleIds, setBibleIds] = useState([]);
 
-    useEffect(() => {
+    useEffect(()=>{
         const fetchBibleIds = async () => {
             try {
                 const response = await fetch('https://api.scripture.api.bible/v1/bibles');
@@ -22,48 +20,21 @@ const BibleSearch = () => {
         fetchBibleIds();
     }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await
-                    fetch(`https://api.scripture.api.bible/v1/bibles/${bibleId}/search?query=${searchTerm}`);
-                const data = await response.json();
-                setSearchResults(data.results);
-            } catch (error) {
-                setError(error)
-            }
-        };
-        if (bibleId) {
-            fetchData();
+    const handleSearch = async (e) =>{
+        e.preventDefault();
+        try{
+            const response = await fetch(`https://api.scripture.api.bible/v1/bibles/${bibleId}/search?query=${searchTerm}`);
+            const data = await response.json();
+            setSearchResults(data.results);
+        } catch (error) {
+            setError(error);
         }
-    }, [bibleId, searchTerm]);
+    };
+
 
     return (
         <>
-            <div>
-                <form>
-                    <label htmlFor="bible-select">Selecteer een Bijbel:</label>
-                    <select id="bible-select" value={bibleId} onChange={e => setBibleId(e.target.value)}>
-                        <option value="">Selecteer een Bijbel</option>
-                        {bibleIds.map(id => (
-                            <option key={id} value={id}>
-                                {id}
-                            </option>
-                        ))}
-                    </select>
-                    <br/>
-                    <label htmlFor="search-input">Zoeken:</label>
-                    <input type="text" id="search-input" value={searchTerm}
-                           onChange={e => setSearchTerm(e.target.value)}/>
-                </form>
-                {error && <p> Er is een fout opgetreden: {error.message}</p>}
-                {searchResults.map(result => (
-                    <div key={result.id}>
-                        <h2>{result.reference}</h2>
-                        <p>{result.content}</p>
-                    </div>
-                ))}
-            </div>
+
         </>
     );
 };
